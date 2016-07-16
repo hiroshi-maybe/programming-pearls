416e0eda122260b90800485cb1090036de807ee0
Hiroshi Kori <hiroshi.maybe@gmail.com>
2016-07-16 14:53:58 -0700

    solve problem 8.2

---
+++ column-8/solution-8.2-bitCountTable.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <iostream>     // std::cout
+#include <algorithm>    // std::unique, std::distance
+#include <vector>       // std::vector
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <math.h>
+
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+using namespace std;
+
+/*
+  $ clang++ -std=c++11 -stdlib=libc++ max-sublist-dp.cpp
+  (No optimization option)
+  - real  0m0.011s
+  - user  0m0.002s
+  - sys   0m0.002s
+
+  $ clang++ -std=c++11 -stdlib=libc++ -Os max-sublist-dp.cpp
+  (-Os optimiztion)
+  - real  0m0.011s
+  - user  0m0.002s
+  - sys   0m0.002s
+*/
+
+#define EXPONENT 10
+
+int countOnBit(int n, int len, int countTable[]) {
+  if (countTable[n] != -1) return countTable[n];
+  
+  int leftLen = len / 2;
+  int rightLen = len - leftLen;
+
+  int left  = n >> rightLen;
+  int fullbit = pow(2, rightLen)-1;
+  int right = n & fullbit;
+
+  int leftCount  = countOnBit(left,  leftLen,  countTable);
+  int rightCount = countOnBit(right, rightLen, countTable);
+  
+  return leftCount + rightCount;
+}
+
+int main () {
+  int N = pow(2, EXPONENT);
+  int countTable[N];
+  memset(countTable, -1, sizeof(countTable));
+  countTable[0] = 0; countTable[1] = 1;    
+  
+  FOR(i, 2, N) {
+    countTable[i] = countOnBit(i, EXPONENT, countTable);
+    cout << i << ':' << countTable[i] << endl;
+  }
+
+  return 0;
+}
